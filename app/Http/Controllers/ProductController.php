5f409d3bb352504edc2a45747944fb6e53a2a219
ilham fakih hilmi <ilhamfakih50@gmail.com>
2024-05-18 22:28:00 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return view('admin.product.home');
+    }
+
+    public function create()
+    {
+        return view('admin.product.create');
+    }
+
+    public function save(Request $request)
+    {
+        $validation = $request->validate([
+            'title' => 'required',
+            'category' => 'required',
+            'price' => 'required',
+        ]);
+        $data = Product::create($validation);
+        if ($data) {
+            session()->flash('success', 'Product Add Successfully');
+            return redirect(route('admin/products'));
+        } else {
+            session()->flash('error', 'Some Problem occure');
+            return redirect(route('admin.products/create'));
+        }
+    }
+}
