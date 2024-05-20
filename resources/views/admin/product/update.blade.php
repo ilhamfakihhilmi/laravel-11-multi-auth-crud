ebdbd63ab8864622c5fcd0a2aaee11985ffbc51d
ilham fakih hilmi <ilhamfakih50@gmail.com>
2024-05-20 17:54:24 +0700

    update

--- resources/views/admin/product/update.blade.php
+++ resources/views/admin/product/update.blade.php
@@ -17,27 +17,30 @@
                         <div class="row">
                             <div class="col mb-3">
                                 <label class="form-label">Product Name</label>
-                                <input type="text" name="title" class="form-control" placeholder="Title"
-                                    value="{{ $products->title }}">
+                                <input type="text" name="title" class="form-control" placeholder="Title" value="{{$products->title}}">
+                                @error('title')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
                             </div>
                         </div>
-
                         <div class="row">
                             <div class="col mb-3">
                                 <label class="form-label">Category</label>
-                                <input type="text" name="category" class="form-control" placeholder="Category"
-                                    value="{{ $products->category }}">
+                                <input type="text" name="category" class="form-control" placeholder="Category" value="{{$products->category}}">
+                                @error('category')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
                             </div>
                         </div>
-
                         <div class="row">
                             <div class="col mb-3">
                                 <label class="form-label">Price</label>
-                                <input type="text" name="price" class="form-control" placeholder="Product Price"
-                                    value="{{ $products->price }}">
+                                <input type="text" name="price" class="form-control" placeholder="Product Price" value="{{$products->price}}">
+                                @error('price')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
                             </div>
                         </div>
-
                         <div class="row">
                             <div class="d-grid">
                                 <button class="btn btn-warning">Update</button>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -145,6 +145,42 @@
                     </div>
                 </div>
             </div>
+
+            <div class="row text-center">
+                <div class="col-4 box1 pt-4">
+                    <div class="span8">
+                        <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0"
+                            marginwidth="0"
+                            src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=15+Springfield+Way,+Hythe,+CT21+5SH&aq=t&sll=52.8382,-2.327815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed"></iframe>
+                    </div>
+                    <a href="tel:+123456789"><i class="fas fa-phone fa-3x"></i>
+                        <h3 class="d-none d-lg-block d-xl-block">Alamat 1</h3>
+                        <p class="d-none d-lg-block d-xl-block">+123456789</p>
+                    </a>
+                </div>
+                <div class="col-4 box2 pt-4">
+                    <div class="span8">
+                        <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0"
+                            marginwidth="0"
+                            src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=15+Springfield+Way,+Hythe,+CT21+5SH&aq=t&sll=52.8382,-2.327815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed"></iframe>
+                    </div>
+                    <a href=""><i class="fas fa-home fa-3x"></i>
+                        <h3 class="d-none d-lg-block d-xl-block">Alamat 2</h3>
+                        <p class="d-none d-lg-block d-xl-block">Piazza del Colosseo, 1, 00184 Roma RM</p>
+                    </a>
+                </div>
+                <div class="col-4 box3 pt-4">
+                    <div class="span8">
+                        <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0"
+                            marginwidth="0"
+                            src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=15+Springfield+Way,+Hythe,+CT21+5SH&aq=t&sll=52.8382,-2.327815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed"></iframe>
+                    </div>
+                    <a href="mailto:test@test.com"><i class="fas fa-envelope fa-3x"></i>
+                        <h3 class="d-none d-lg-block d-xl-block">Alamat 3</h3>
+                        <p class="d-none d-lg-block d-xl-block">test@test.com</p>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
